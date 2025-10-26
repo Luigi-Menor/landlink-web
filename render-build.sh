1a309ec7362d607ee0117598881bd42450c6b4d5
Luigi <luigicordoba11@gmail.com>
2025-10-26 18:13:51 -0500

    Agrego script para instalar drivers de SQL Server en Render

---
+++ render-build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y unixodbc-dev gcc g++ curl
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
